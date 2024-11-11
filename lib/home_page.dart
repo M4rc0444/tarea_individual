0915ee531b1ddb1732d6bea41907123a5809e2fe
M4rc0444 <marcouavp@gmail.com>
2024-11-11 07:32:17 -0600

    corregido_home_agregado_mybutton

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,9 +1,9 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:tarea_individual_marcoavila/const.dart';
-import 'package:tarea_individual_marcoavila/utils/my_button.dart';
-import 'package:tarea_individual_marcoavila/utils/result_message.dart';
+import 'package:tarea_individual/const.dart';
+import 'package:tarea_individual/utils/my_button.dart';
+import 'package:tarea_individual/utils/result_message.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
