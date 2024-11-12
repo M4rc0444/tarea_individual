fce739252c80fedb5b339ca3a74f4ee41c6832a8
M4rc0444 <marcouavp@gmail.com>
2024-11-12 06:16:48 -0600

    agregado_mensaje de error

---
+++ lib/utils/result_message.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+import '../const.dart';
+
+class ResultMessage extends StatelessWidget {
+  final String message;
+  final VoidCallback onTap;
+  final icon;
+
+  ResultMessage({
+    Key? key,
+    required this.message,
+    required this.onTap,
+    required this.icon,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      backgroundColor: Colors.deepPurple,
+      content: Container(
+        height: 200,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(
+              message,
+              style: whiteTextStyle,
+            ),
+            //BOTON PARA LA SIGUIENTE PREGUNTA
+            GestureDetector(
+              onTap: onTap,
+              child: Container(
+                padding: EdgeInsets.all(4),
+                decoration: BoxDecoration(
+                    color: Colors.deepPurple[300],
+                    borderRadius: BorderRadius.circular(10)),
+                child: Icon(
+                  icon,
+                  color: Colors.white,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
